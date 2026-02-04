4949497f3a29c7f7ea8c407b48f8790298836180
Ken-fighter <3434492377@qq.com>
2026-02-04 10:44:01 +0800

    Fix function signature formatting in cn function

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,3 +1,4 @@
-export function cn(...classes: (string | boolean | undefined | null)[]): string {
+export function cn(...classes: (string | boolean | undefined | null)[]): 
+string {
   return classes.filter(Boolean).join(' ');
 }
